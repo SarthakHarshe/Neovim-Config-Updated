e6d1ce3547f799e5c5f6c114d331a304d3b2236d
Sarthak Harshe <sarthak@Sarthaks-MacBook-Pro.local>
2024-10-09 01:37:48 -0700

    Add neovim configuration folder

---
+++ nvim/lua/sarthak/plugins/lsp/mason.lua
@@ -0,0 +1,61 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tsp_server",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"angularls",
+				"clangd",
+				"cmake",
+				"eslint",
+				"jsonls",
+				"ast_grep",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"clang-format", -- C/CPP formatter
+				"pylint", -- Python linter
+				"cpplint", -- C/CPP linter
+			},
+		})
+	end,
+}
